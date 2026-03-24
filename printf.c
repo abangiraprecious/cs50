ffaf28ccb8b20f0374fa8b5d228b0987879c62fa
Precious Abangira <abangiraprecious@gmail.com>
2026-03-24 21:19:30 +0300

    print

---
+++ printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World\n");
+
+    int a = 3;
+    int b = 4;
+
+    int sum = a + b;
+
+    printf("Sum is: %d\n", sum);
+
+    return 0;
+
+}
